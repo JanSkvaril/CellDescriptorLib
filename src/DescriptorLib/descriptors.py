260de329f6acfa4060ae554072e17e71d50973f1
Jan Skvaril <kondr4@seznam.cz>
2023-11-13 09:57:09 +0100

    Implemented histogram features

--- src/DescriptorLib/descriptors.py
+++ src/DescriptorLib/descriptors.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from scipy.stats import moment, gmean,skew,kurtosis, entropy
 
 def StdDev(image: np.array, mask: np.array) -> float:
     """
@@ -12,3 +12,60 @@ def StdDev(image: np.array, mask: np.array) -> float:
     src = np.float64(np.copy(image))
     src[mask == 0] = np.nan
     return np.nanstd(src)
+
+
+
+def Histogram(image : np.array, mask : np.array, bins:int|None = None) -> np.array:
+    """
+        Computes the histogram of the image within the mask.
+        - image: 2D numpy array
+        - mask: 2D numpy array, binary mask
+        - bins: number of bins for the histogram, if none, max value of image is used
+        Returns a 1D numpy array with the histogram.
+    """
+    if bins is None:
+        bins = np.max(image)
+    return np.histogram(image, weights=mask,bins=bins)[0]
+
+
+def HistogramDescriptors(histogram : np.array) -> dict:
+    """
+        Computes the descriptors of the histogram.
+        - histogram: 1D numpy array, histogram
+        Returns a dictionary with the following descriptors:
+        - mean
+        - std
+        - var
+        - median
+        - max
+        - min
+        - argmax
+        - moment3
+        - geometric_mean
+        - skewness
+        - kurtosis
+        - entropy
+        - energy
+        - smoothness
+
+    """
+    result = dict()
+
+    dist = histogram/ np.sum(histogram)
+    result["mean"] = np.mean(histogram)
+    result["std"] = np.std(dist)
+    result["var"] = np.var(dist)
+    result["median"] = np.median(histogram)
+    result["max"] = np.max(histogram)
+    result["min"] = np.min(histogram)
+    result["argmax"] = np.argmax(histogram)
+    result["moment3"] = moment(dist, moment=3)
+    result["geometric_mean"] = gmean(histogram)
+    result["skewness"] = skew(dist)
+    result["kurtosis"] = kurtosis(dist)
+    result["entropy"] = entropy(dist)
+    result["energy"] = np.sum(np.square(dist))
+    result["smoothness"] = 1 - 1/(1 +  result["std"]**2)
+
+    
+    return result
