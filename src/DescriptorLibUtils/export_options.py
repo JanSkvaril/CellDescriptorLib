bc6886449763bf06771461dee0371af8bd267c35
Denisa Rudincová <xrudinc@fi.muni.cz>
2024-04-23 11:42:33 +0200

    Export: class edits

--- src/DescriptorLibUtils/export_options.py
+++ src/DescriptorLibUtils/export_options.py
@@ -4,12 +4,12 @@ class ExportOptions:
     Class for export options.
     """
     def __init__(self):
-        self.export_json = 1
-        self.export_pickle = 1
-        self.export_region_imgs = 1
-        self.mode_3d = 0
-        self.remove_background = 0
-        self.crop_offset = 0
+        self.__export_json = 1
+        self.__export_pickle = 1
+        self.__export_region_imgs = 1
+        self.__mode_3d = 0
+        self.__remove_background = 0
+        self.__crop_offset = 0
 
     def __str__(self):
         return (f"Export JSON: {self.export_json}\n"
@@ -21,38 +21,38 @@ class ExportOptions:
 
     # setters
     def set_export_json(self, value: int) -> None:
-        self.export_json = value
+        self.__export_json = value
 
     def set_export_pickle(self, value: int) -> None:
-        self.export_pickle = value
+        self.__export_pickle = value
 
     def set_export_region_imgs(self, value: int) -> None:
-        self.export_region_imgs = value
+        self.__export_region_imgs = value
 
     def set_mode_3d(self, value: int) -> None:
-        self.mode_3d = value
+        self.__mode_3d = value
 
     def set_remove_background(self, value: int) -> None:
-        self.remove_background = value
+        self.__remove_background = value
 
     def set_crop_offset(self, value: int) -> None:
-        self.crop_offset = value
+        self.__crop_offset = value
 
     # getters
     def get_export_json(self) -> int:
-        return self.export_json
+        return self.__export_json
 
     def get_export_pickle(self) -> int:
-        return self.export_pickle
+        return self.__export_pickle
 
     def get_export_region_imgs(self) -> int:
-        return self.export_region_imgs
+        return self.__export_region_imgs
 
     def get_mode_3d(self) -> int:
-        return self.mode_3d
+        return self.__mode_3d
 
     def get_remove_background(self) -> int:
-        return self.remove_background
+        return self.__remove_background
 
     def get_crop_offset(self) -> int:
-        return self.crop_offset
+        return self.__crop_offset
