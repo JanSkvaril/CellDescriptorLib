3f44eab8538778ac8c981a134e1bb3aed66e6663
Simona Sarvasova <sarvasovasimona@gmail.com>
2024-04-29 15:59:33 +0200

    new 3d desc: stdev, mean, hist desc

--- src/DescriptorLib/descriptors3d.py
+++ src/DescriptorLib/descriptors3d.py
@@ -1,9 +1,9 @@
 import numpy as np
-from math import pi
+from math import pi, sqrt
 from scipy.spatial import ConvexHull
 from porespy.metrics import regionprops_3D
 
-from .descriptor import DescriptorBase, DescriptorType
+from descriptor import DescriptorBase, DescriptorType
 
 
 class MaskDescriptors3D(DescriptorBase):
@@ -53,3 +53,130 @@ class MaskDescriptors3D(DescriptorBase):
 
     def GetType(self) -> DescriptorType:
         return DescriptorType.DICT_SCALAR
+
+
+class Mean3D(DescriptorBase):
+    """
+        Calculates the mean of the image within the mask.
+        - image: 3D numpy array
+        - mask: 3D numpy array, binary mask
+        Returns scalar representing mean.
+    """
+
+    def Eval(self, image: np.array, mask: np.array):
+        image_copy = np.copy(image)
+        image_copy[mask == 0] = 0
+
+        return np.sum(image_copy) / np.count_nonzero(mask)
+
+    def GetName(self) -> str:
+        return "Mean"
+
+    def GetType(self) -> DescriptorType:
+        return DescriptorType.SCALAR
+
+
+class StdDev3D(DescriptorBase):
+    """
+        Calculates the standard deviation of the image within the mask.
+        - image: 3D numpy array
+        - mask: 3D numpy array, binary mask
+        Returns scalar representing standard deviation.
+    """
+
+    def Eval(self, image: np.array, mask: np.array):
+        non_zero_points = np.copy(image)[mask != 0]
+
+        count_points = np.count_nonzero(mask)
+        mean = np.sum(non_zero_points) / count_points
+
+        src = non_zero_points.flatten()
+
+        return sqrt(np.sum((src - mean) ** 2) / count_points)
+
+    def GetName(self) -> str:
+        return "StdDev"
+
+    def GetType(self) -> DescriptorType:
+        return DescriptorType.SCALAR
+
+
+class Histogram3D(DescriptorBase):
+    """
+        Computes the histogram of the image within the mask.
+        - image: 3D numpy array
+        - mask: 3D numpy array, binary mask
+        - bins: number of bins for the histogram, if none, max value of image
+          is used
+        Returns a 1D numpy array with the histogram.
+    """
+
+    def __init__(self, bins: int | None = None):
+        self.bins = bins
+
+    def Eval(self, image: np.array, mask: np.array):
+        return self.Histogram3D(image, mask, bins=self.bins)
+
+    def Histogram3D(self,
+                    image: np.array,
+                    mask: np.array,
+                    bins: int | None = None) -> np.array:
+        if bins is None:
+            bins = np.max(image)
+
+        return np.histogram(image[mask != 0], bins)
+
+    def GetName(self) -> str:
+        return "Histogram"
+
+    def GetType(self) -> DescriptorType:
+        return DescriptorType.VECTOR
+
+
+class HistogramDescriptors3D(DescriptorBase):
+    """
+        Computes the descriptors of the histogram.
+        - histogram: 1D numpy array, histogram
+        Returns a dictionary with the following descriptors:
+        - mean
+        - std
+        - var
+        - median
+        - max
+        - min
+        - argmax
+        - moment3
+        - geometric_mean
+        - skewness
+        - kurtosis
+        - entropy
+        - energy
+        - smoothness
+
+    """
+    def __init__(self, bins: int | None = None):
+        self.bins = bins
+
+    def Eval(self, image: np.array, mask: np.array):
+        histogram, bin_edges = Histogram3D(self.bins).Eval(image, mask)
+        return self.HistogramDescriptors3D(histogram, bin_edges)
+
+    def GetName(self) -> str:
+        return "Histogram descriptors"
+
+    def GetType(self) -> DescriptorType:
+        return DescriptorType.DICT_SCALAR
+
+    def HistogramDescriptors3D(self,
+                               histogram: np.array,
+                               bin_edges: np.array) -> dict:
+        result = dict()
+
+        bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
+        result["mean"] = np.sum(bin_centers * histogram) / np.sum(histogram)
+        result["var"] = np.sum(((bin_centers - result["mean"]) ** 2) * histogram) / np.sum(histogram)
+        result["std"] = sqrt(result["var"])
+        result["max"] = bin_edges[len(bin_edges) - 1]
+        result["min"] = bin_edges[0]
+
+        return result
